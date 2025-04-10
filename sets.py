601c2fdce5d4dc978e8f353daee11dd989723e8f
qwertyfull <aqsahussain126@gmail.com>
2025-04-10 11:43:31 +0500

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,83 @@
+
+# task 1 UNION 
+
+A = { 2, 3, 4}
+B = { 5, 6, 7}
+# print(A.union(B))
+
+# task 2 iNTERSECTION
+
+# print(A.intersection(B))
+
+# task 3 Difference
+
+# print(A.difference(B))
+
+# task 4 Add element
+
+# B.add(0)
+# print(B)
+
+# task 5 update
+
+# A.update(B)
+# print(A)
+
+# TASK 6 remove element
+
+# B.remove(7)
+# print(B)
+
+# TASK 1: find len 
+# A = { 10,20,30}
+# B = { 40, 50,60}
+# print(len(A))
+# print(len(B))
+
+# task 2: creat empty set 
+# A = { 10,20,30}
+# empty_set = set()
+# print(type(empty_set))  # <class 'set'>
+
+
+# task 3 union
+# A = { 10,20,30}
+# B = { 40,50,60}
+# C = { 2 ,3,4 }
+# print(A.union(B,C))
+
+# TASK 4: find common elements 
+# A = { 10,20,30}
+# B = { 40,20,60}
+# print(A.intersection(B))
+ 
+ # TASK 5 union
+# A = { 10,20,30}
+# B = { 40,20,60}
+# print(A.union(B))
+
+# TASK 6 find difference
+# A = {1,3,5}
+# B = {2,4,6}
+# print(A.difference(B)) 
+
+# TASK 7 symmetric difference
+# A = {2,3,4,5,6}
+# B = {8,2,5,6,0}
+# print(A.symmetric_difference(B))
+
+# # task 8 
+# A = {2,3,4,5,6}
+# A.add("charlie")
+# print(A)
+
+# # task 9 
+# A = {2,3,4,5,6}
+# B = {8,2,5,6,0}
+
+# c = A.copy()
+# c.update(B)
+# print(c)
+
+# B.add(40)
+# print(B)
